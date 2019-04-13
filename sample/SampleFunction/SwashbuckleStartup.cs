70e8123a8e077afd0a49aba02099271451df74a1
yuka1984 <yuka.abn@sigmact.com>
2019-04-13 23:31:12 +0900

    Add Sample Functions

---
+++ sample/SampleFunction/SwashbuckleStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using AzureFunctions.Extensions.Swashbuckle;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using SampleFunction;
+
+[assembly: WebJobsStartup(typeof(SwashBuckleStartup))]
+namespace SampleFunction
+{
+    internal class SwashBuckleStartup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            //Register the extension
+            builder.AddSwashBuckle(Assembly.GetExecutingAssembly());
+
+        }
+    }
+}
