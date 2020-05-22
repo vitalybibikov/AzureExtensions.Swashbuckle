aeaf328feaa01a257bff5cf879f26c23219e07c6
Vitali Bibikov <bibikovvitaly@gmail.com>
2020-05-22 20:42:10 +0300

    Fix #15

--- src/AzureFunctions.Extensions.Swashbuckle/AzureFunctions.Extensions.Swashbuckle/SwashBuckle/Filters/Mapper/TypeMapper.cs
+++ src/AzureFunctions.Extensions.Swashbuckle/AzureFunctions.Extensions.Swashbuckle/SwashBuckle/Filters/Mapper/TypeMapper.cs
@@ -24,6 +24,9 @@ namespace AzureFunctions.Extensions.Swashbuckle.SwashBuckle.Filters.Mapper
             },
             {
                 typeof(bool), "boolean"
+            },
+            {
+                typeof(string), "string"
             }
         };
 
