b86916b0de1c56313b6e4ab3656c76884d683783
Vitali Bibikov <bibikovvitaly@gmail.com>
2026-03-09 15:43:21 +0100

    revert: restore full Program.cs with swagger configuration

--- src/AzureFunctions.Extensions.Swashbuckle/TestFunction/Program.cs
+++ src/AzureFunctions.Extensions.Swashbuckle/TestFunction/Program.cs
@@ -1,7 +1,73 @@
+using System;
+using System.Collections.Generic;
 using Microsoft.Extensions.Hosting;
+using System.Reflection;
+using AzureFunctions.Extensions.Swashbuckle.Settings;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi;
+using AzureFunctions.Extensions.Swashbuckle;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
+    .ConfigureServices((hostContext, services) =>
+    {
+        services.AddSwashBuckle(opts =>
+        {
+            opts.RoutePrefix = "api";
+            opts.SpecVersion = OpenApiSpecVersion.OpenApi3_0;
+            opts.AddCodeParameter = true;
+            opts.PrependOperationWithRoutePrefix = true;
+            opts.XmlPath = "TestFunction.xml";
+            opts.Documents = new[]
+            {
+                new SwaggerDocument
+                {
+                    Name = "v1",
+                    Title = "Swagger document",
+                    Description = "Swagger test document",
+                    Version = "v2"
+                },
+                new SwaggerDocument
+                {
+                    Name = "v2",
+                    Title = "Swagger document 2",
+                    Description = "Swagger test document 2",
+                    Version = "v2"
+                }
+            };
+            opts.Title = "Swagger Test";
+            opts.ConfigureSwaggerGen = x =>
+            {
+                x.CustomOperationIds(apiDesc => apiDesc.TryGetMethodInfo(out MethodInfo methodInfo)
+                    ? methodInfo.Name
+                    : new Guid().ToString());
+
+                x.AddSecurityDefinition("oauth2",
+                    new OpenApiSecurityScheme
+                    {
+                        Type = SecuritySchemeType.OAuth2,
+                        Flows = new OpenApiOAuthFlows
+                        {
+                            Implicit = new OpenApiOAuthFlow
+                            {
+                                AuthorizationUrl = new Uri("https://your.idserver.net/connect/authorize"),
+                                Scopes = new Dictionary<string, string>
+                                {
+                                    { "api.read", "Access read operations" },
+                                    { "api.write", "Access write operations" }
+                                }
+                            }
+                        }
+                    });
+            };
+
+            opts.ClientId = "your.client.id";
+            opts.ClientSecret = "your.client.secret";
+            opts.UsePkceWithAuthorizationCodeGrant = true;
+            opts.OAuth2RedirectPath = "http://localhost:7071/api/swagger/oauth2-redirect";
+        });
+    })
     .Build();
 
 host.Run();
